e10fae722b5ac6d68b697df51445da4e18a35735
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-02-09 09:51:08 +0000

    adding bookmarks feature added and tested

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,27 @@
 require 'pg'
-class Bookmark
-    def self.all
 
-        connection = PG.connect(dbname: 'bookmark_manager')
-        result = connection.exec("SELECT * FROM bookmarks;")
-        result.map { |bookmark| bookmark['url'] }
+class Bookmark
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
     end
+
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map { |bookmark| bookmark['url'] }
   end
+
+  def self.create(url:)
+    if ENV['ENVIRONMENT'] == 'test'
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+      else
+        connection = PG.connect(dbname: 'bookmark_manager')
+      end
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+
+  end
+
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,18 @@
+require_relative './setup_test_database'
+
+ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
+
+ENV['ENVIRONMENT'] = 'test'
+
 ENV['RACK_ENV'] = 'test'
+
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -7,6 +21,8 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
+
 
 # Tell Capybara to talk to BookmarkManager
 Capybara.app = BookmarkManager
